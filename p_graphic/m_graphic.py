7b4b2582d3231cf9e3dec256732565232ce3d32a
carpiero <carpiero@gmail.com>
2020-10-14 21:54:43 +0200

    error

--- p_graphic/m_graphic.py
+++ p_graphic/m_graphic.py
@@ -13,7 +13,7 @@ def graphic():
     df_final_pob_dropdown_c = pd.read_parquet('./data/main_processed/df_final_pob_melt_PC.parquet')
     df_final_pob_dropdown_c = df_final_pob_dropdown_c.loc[df_final_pob_dropdown_c['coste_efectivo_PC']>=1]
     df_final_pob_dropdown_c = df_final_pob_dropdown_c[['Nombre Ente Principal','Descripción']]
-    df_final_pob_dropdown_c['Descripción'] = df_final_pob_dropdown_c['Descripción'].str.replace(r'^...' , '')
+
 
     df_final_pob_dropdown_c.to_parquet('./data/main_processed/df_final_pob_dropdown_c.parquet')
 
